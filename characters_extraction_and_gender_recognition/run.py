0b5b6f73adb4df4ea32ef50aa6233febff100c3b
Tommaso Battisti <tommasobattisti@icloud.com>
2022-08-19 18:51:54 +0200

    Update run.py

--- characters_extraction_and_gender_recognition/run.py
+++ characters_extraction_and_gender_recognition/run.py
@@ -20,8 +20,8 @@ for path in directory_paths:
 #for idx, filepath in enumerate(file_paths):
 #    print(idx, filepath)
 
-ncl = gender_recognition(get_characters(open(file_paths[14]).read()))
-print(ncl)
+#ncl = gender_recognition(get_characters(open(file_paths[14]).read()))
+#print(ncl)
 
 
 #or in this way:
@@ -40,3 +40,10 @@ for file_path in tqdm(file_paths): #tqdm just displays a progress bar in the ter
 for key in characters:
     print(key, '-->', characters[key])
 """
+
+
+
+
+set0 = {'bill', 'jim', 'avast', 'hunter', 'ben', 'trelawney', 'dick', 'livesey', 'scatter', 'george', 'dogger', 'harry', 'tom', 'benbow', 'obrien', 'alan', 'billy', 'barbecue', 'jack', 'hawkins', 'jim hawkins', 'long john silver', 'captain smollett', 'mr dance', 'capn trelawney', 'george merry', 'mr hands', 'mr arrow', 'mr smollett', 'long john', 'black dog', 'john silver', 'captain kidd', 'mr trelawney', 'benjamin gunn', 'abraham gray', 'captain flint', 'doctor livesey', 'ben gunn', 'capn hawkins', 'tom morgan', 'master silver', 'capn smollett', 'master pew', 'mr hawkins', 'mr silver', 'billy bones', 'davy jones', 'tom redruth', 'job anderson'}
+set1 = {'bill', 'ben', 'barbecue', 'morgan', 'jack', 'obrien', 'jim', 'hawkins', 'dogger', 'billy', 'alan', 'hunter', 'george', 'tom', 'pew', 'dick', 'benbow', 'captain kidd', 'captain flint', 'davy jones', 'mr silver', 'black dog', 'capn hawkins', 'long john silver', 'capn trelawney', 'mr smollett', 'george merry', 'tom morgan', 'doctor livesey', 'benjamin gunn', 'mr arrow', 'master silver', 'captain smollett', 'billy bones', 'mr trelawney', 'mr hands', 'mr hawkins', 'jim hawkins', 'john silver', 'capn smollett', 'job anderson', 'long john', 'mr dance', 'abraham gray', 'tom redruth', 'ben gunn'}
+print(set0 - set1)
