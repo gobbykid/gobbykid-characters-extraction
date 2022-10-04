ebfc2a934222ecfd2e5db47cb5f2b0bb9cdaa56d
Tommaso Battisti <tommasobattisti@icloud.com>
2022-10-04 20:25:04 +0200

    Update genderRec.py

--- characters_extraction_and_gender_recognition/genderRec.py
+++ characters_extraction_and_gender_recognition/genderRec.py
@@ -1,6 +1,17 @@
 from genderize import Genderize
 from genderComputer import GenderComputer
 import re
+import csv
+
+
+with open('female_chars.csv') as f:
+    reader = csv.reader(f)
+    already_classified_female_characters = [row[0] for row in reader]
+
+with open('male_chars.csv') as f:
+    reader = csv.reader(f)
+    already_classified_male_characters = [row[0] for row in reader]
+
 
 male_words = {"sir", "duke", "lord", "king", "prince",  "mister", "mr", "father", "uncle", "son", "brother", "boy", "widower", "master"}
 female_words = {"lady", "duchess", "queen", "princess", "dame", "missis", "miss", "mrs", "ms", "aunt", "mother", "sister", "daughter", "girl", "widow", "mistress"}
@@ -45,101 +56,113 @@ def gender_recognition(names_set):
     female_characters = []
     unknown_gender = []
 
-    for name in single_word_names:                   #First check by means of genderComputer on the list of names composed by a single word
-        gender = gc.resolveGender(name, None)       #If the name is not found, it returns None
-        if gender == "male": 
+
+    for name in single_word_names:                      #_______________________________________NEW BLOCK FOR THE BRANCH
+        if name in already_classified_male_characters: #If the name is already classified as a male character, it is added to the list of male characters
             male_characters.append(name)
-        elif gender == "female":
-            female_characters.append(name)
-        else:
-            try:                                      #If genderComputer is not able to guess the gender, Genderize is used
-                char_info = Genderize().get([name])   #Genderize returns a list of dictionaries, so we need to access the first element of the list
-                if char_info[0]["gender"] == "male":
-                    male_characters.append(name)
-                elif char_info[0]["gender"] == "female":
-                    female_characters.append(name)
-                else:
+        elif name in already_classified_female_characters: #If the name is already classified as female character, it is added to the list of female characters
+            female_characters.append(name)              #_______________________________________END OF THE NEW BLOCK FOR THE BRANCH
+        else:                                            #check by means of genderComputer on the list of names composed by a single word
+            gender = gc.resolveGender(name, None)       #If the name is not found, it returns None
+            if gender == "male": 
+                male_characters.append(name)
+            elif gender == "female":
+                female_characters.append(name)
+            else:
+                try:                                      #If genderComputer is not able to guess the gender, Genderize is used
+                    char_info = Genderize().get([name])   #Genderize returns a list of dictionaries, so we need to access the first element of the list
+                    if char_info[0]["gender"] == "male":
+                        male_characters.append(name)
+                    elif char_info[0]["gender"] == "female":
+                        female_characters.append(name)
+                    else:
+                        unknown_gender.append(name)
+                except:
                     unknown_gender.append(name)
-            except:
-                unknown_gender.append(name)
 
     
-    for full_name in multiple_words_names:                        #The first block of checks aims at guessing the gender on the basis of genderdized words present in the strings without using any additional library
+    for full_name in multiple_words_names:                        
         joined_name = " ".join(full_name)
 
-        if len(set(full_name) & male_words) > 0:                #By checking the presence of genderdized words in the name we can directly append the name in the right list
-            do_not_add = False
-            for masculine_name in male_characters:
-                if re.match(".+\s.+", masculine_name):
-                    mnw_list = masculine_name.split(" ")
-                    if full_name[1:] == mnw_list[1:] or full_name[1:] == mnw_list:
-                        do_not_add = True
-                        break
-                else:
-                    continue
-            if do_not_add == False:
-                male_characters.append(joined_name)
-
-
-        elif len(set(full_name) & female_words) > 0:
+        if joined_name in already_classified_male_characters: #_______________________________________NEW BLOCK FOR THE BRANCH
+            male_characters.append(joined_name)
+        elif joined_name in already_classified_female_characters:
+            female_characters.append(joined_name)              #_______________________________________END OF THE NEW BLOCK FOR THE BRANCH
+        else:
+                                                                #The block of checks aims at guessing the gender on the basis of genderdized words present in the strings without using any additional library
+            if len(set(full_name) & male_words) > 0:                #By checking the presence of genderdized words in the name we can directly append the name in the right list
                 do_not_add = False
-                for feminine_name in female_characters:
-                    if re.match(".+\s.+", feminine_name):
-                        mnw_list = feminine_name.split(" ")
+                for masculine_name in male_characters:
+                    if re.match(".+\s.+", masculine_name):
+                        mnw_list = masculine_name.split(" ")
                         if full_name[1:] == mnw_list[1:] or full_name[1:] == mnw_list:
                             do_not_add = True
                             break
                     else:
-                        continue                   
+                        continue
                 if do_not_add == False:
-                    female_characters.append(joined_name)
+                    male_characters.append(joined_name)
 
 
-        else:                                                       #Now, it is used the same process as before: first we check the gender by meand of genderComputer
-            gender = gc.resolveGender(joined_name, None)           #If the name is not found, it returns None
-            if gender == "male":
-                male_characters = check_list_mwn(male_characters, full_name)
-
-            elif gender == "female":
-                female_characters = check_list_mwn(female_characters, full_name)
-            else:                                       #In the following block the detection is based on each word composing the names
-                already_added = False                  
-                for single_name in full_name:
-                    if already_added:
-                        break
-                    else:
-                        if single_name in male_characters:
-                            male_characters = check_list_mwn(male_characters, full_name)
-                            already_added = True
-                        elif single_name in female_characters:
-                            female_characters = check_list_mwn(female_characters, full_name)
-                            already_added = True
-                        elif single_name in unknown_gender:
-                            continue
+            elif len(set(full_name) & female_words) > 0:
+                    do_not_add = False
+                    for feminine_name in female_characters:
+                        if re.match(".+\s.+", feminine_name):
+                            mnw_list = feminine_name.split(" ")
+                            if full_name[1:] == mnw_list[1:] or full_name[1:] == mnw_list:
+                                do_not_add = True
+                                break
                         else:
-                            name_gender = gc.resolveGender(single_name, None) 
-                            if name_gender == "male":
+                            continue                   
+                    if do_not_add == False:
+                        female_characters.append(joined_name)
+
+
+            else:                                                       #Now, it is used the same process as before: first we check the gender by meand of genderComputer
+                gender = gc.resolveGender(joined_name, None)           #If the name is not found, it returns None
+                if gender == "male":
+                    male_characters = check_list_mwn(male_characters, full_name)
+
+                elif gender == "female":
+                    female_characters = check_list_mwn(female_characters, full_name)
+                else:                                       #In the following block the detection is based on each word composing the names
+                    already_added = False                  
+                    for single_name in full_name:
+                        if already_added:
+                            break
+                        else:
+                            if single_name in male_characters:
                                 male_characters = check_list_mwn(male_characters, full_name)
                                 already_added = True
-                            elif name_gender == "female":
+                            elif single_name in female_characters:
                                 female_characters = check_list_mwn(female_characters, full_name)
                                 already_added = True
+                            elif single_name in unknown_gender:
+                                continue
                             else:
-                                if full_name.index(single_name) == len(full_name) -1:
-                                    unknown_gender.append(joined_name)
+                                name_gender = gc.resolveGender(single_name, None) 
+                                if name_gender == "male":
+                                    male_characters = check_list_mwn(male_characters, full_name)
+                                    already_added = True
+                                elif name_gender == "female":
+                                    female_characters = check_list_mwn(female_characters, full_name)
+                                    already_added = True
                                 else:
-                                    try:                                                #This try except block is necessary since Genderize has a daily request limit
-                                        name_gender = Genderize().get([single_name])
-                                        if name_gender[0]["gender"] == "male":
-                                            male_characters = check_list_mwn(male_characters, full_name)
-                                            already_added = True
-                                        elif name_gender[0]["gender"] == "female":
-                                            female_characters = check_list_mwn(female_characters, full_name)
-                                            already_added = True
-                                        else:
+                                    if full_name.index(single_name) == len(full_name) -1: #it is checked if the name is the last word of the string because Genderize works only in the first iteration since it takes in input the full name
+                                        unknown_gender.append(joined_name)
+                                    else:
+                                        try:                                                #This try except block is necessary since Genderize has a daily request limit
+                                            name_gender = Genderize().get([single_name])
+                                            if name_gender[0]["gender"] == "male":
+                                                male_characters = check_list_mwn(male_characters, full_name)
+                                                already_added = True
+                                            elif name_gender[0]["gender"] == "female":
+                                                female_characters = check_list_mwn(female_characters, full_name)
+                                                already_added = True
+                                            else:
+                                                continue
+                                        except:
                                             continue
-                                    except:
-                                        continue
                                 
 
 
